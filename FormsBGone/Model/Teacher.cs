ad0a09c08901083619c1fb700bdbecf16e76fbac
Trent Wells <jtrentw143@gmail.com>
2024-09-11 20:43:36 -0500

    Updated models

--- FormsBGone/Model/Teacher.cs
+++ FormsBGone/Model/Teacher.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace FormsBGone.NewFolder;
+namespace FormsBGone.Model;
 
 public partial class Teacher
 {
+    public string Email { get; set; } = null!;
+
     public int TeacherId { get; set; }
 
-    public string? FirstName { get; set; }
+    public string FirstName { get; set; } = null!;
 
     public string? MiddleInitial { get; set; }
 
-    public string? LastName { get; set; }
+    public string LastName { get; set; } = null!;
 
-    public string? Email { get; set; }
+    public string SuperiorEmail { get; set; } = null!;
 
-    public virtual Account? EmailNavigation { get; set; }
+    public virtual Account EmailNavigation { get; set; } = null!;
 
     public virtual ICollection<Student> Students { get; set; } = new List<Student>();
+
+    public virtual Administrator SuperiorEmailNavigation { get; set; } = null!;
 }
