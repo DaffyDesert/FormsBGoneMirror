132e9506b0c81b32013e305f3ff329e3e3b433e7
Trent Wells <jtrentw143@gmail.com>
2024-09-14 12:11:03 -0500

    login is working but we have 2 bugs 1. once logged in, if a user clicks the about page and then home again, they have to log back in.
    2. a user is able to click on the URL and simply add /administrator or /parent and it takes them to that page.

--- FormsBGone/Program.cs
+++ FormsBGone/Program.cs
@@ -1,29 +1,62 @@
 using FormsBGone;
 using FormsBGone.Components;
+using FormsBGone.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure the database context
 builder.Services.AddDbContext<CapstoneContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
+// Add JWT Authentication Service
+builder.Services.AddScoped<JwtAuthenticationService>();
+
+// Configure JWT Authentication
+var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidateLifetime = true
+    };
+});
+
+// Add Razor Components
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication(); // Ensure this is added
+app.UseAuthorization();  // Ensure this is added
+
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
