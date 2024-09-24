bc8e7704d3ecceffbbee71e5fb25291013ddc3fb
DaffyDesert <aj.espersen419@gmail.com>
2024-09-23 20:10:01 -0500

    Scaffolded new DB Changes

---
+++ FormsBGone/Model/FormsByStudent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormsBGone.Model;
+
+public partial class FormsByStudent
+{
+    public int StudentId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? MiddleInitial { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public int Grade { get; set; }
+
+    public int FormId { get; set; }
+
+    public string FormDescription { get; set; } = null!;
+
+    public DateTime? CompletedDate { get; set; }
+
+    public DateTime ExpirationDate { get; set; }
+
+    public string FormStatus { get; set; } = null!;
+
+    public string? FormFilePath { get; set; }
+}
---
+++ FormsBGone/Model/FormsByTeacher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormsBGone.Model;
+
+public partial class FormsByTeacher
+{
+    public string Email { get; set; } = null!;
+
+    public int TeacherId { get; set; }
+
+    public string TeacherFirstName { get; set; } = null!;
+
+    public string? TeacherMiddleInitial { get; set; }
+
+    public string TeacherLastName { get; set; } = null!;
+
+    public string TeacherSuperiorEmail { get; set; } = null!;
+
+    public int StudentId { get; set; }
+
+    public string StudentFirstName { get; set; } = null!;
+
+    public string? StudentMiddleInitial { get; set; }
+
+    public string StudentLastName { get; set; } = null!;
+
+    public int Grade { get; set; }
+
+    public int FormId { get; set; }
+
+    public string FormDescription { get; set; } = null!;
+
+    public DateTime? CompletedDate { get; set; }
+
+    public DateTime ExpirationDate { get; set; }
+
+    public string FormStatus { get; set; } = null!;
+
+    public string? FormFilePath { get; set; }
+}
